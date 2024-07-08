e0eabe5455bb88dbd94fe9044674c80950d26e08
koles <yg@htfv.com>
2024-07-08 23:54:03 +0700

    добавил статику в общий шаблон но пока не работает.

--- blog/settings.py
+++ blog/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,3 +113,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from python_blog import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="main"),
     path("about/", views.about, name="about"),    
     path("blog/", include("python_blog.urls")),    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

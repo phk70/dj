c1da322fdb94f2f55aee9555a97643576e9ab9fa
koles <yg@htfv.com>
2024-07-15 20:50:39 +0700

    установил shell_plus

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,6 +1,7 @@
+from venv import create
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField()    
